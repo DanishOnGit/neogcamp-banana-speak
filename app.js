682cca2cb9aa6cc04174e5b909070c82e073206f
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-02-22 12:52:20 +0530

    refactor: variable declarations

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var translateButton=document.querySelector("#btnTranslate");
-var inputText=document.querySelector("#input");
-var outputText=document.querySelector("#output");
+const translateButton=document.querySelector("#btnTranslate");
+const inputText=document.querySelector("#input");
+const outputText=document.querySelector("#output");
 // var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverUrl="https://api.funtranslations.com/translate/minion.json";
+const serverUrl="https://api.funtranslations.com/translate/minion.json";
 
 //get server url with query which is to be translated
 function getTranslationUrl(text){
@@ -16,11 +16,11 @@ function errorHandler(error){
 
 //processing
 function clickHandler(){
-    var enteredText=inputText.value
+    let enteredText=inputText.value
     fetch(getTranslationUrl(enteredText))
     .then(response=>response.json())
     .then(json=> {
-        var translatedText=json.contents.translated;
+        let translatedText=json.contents.translated;
         outputText.innerText=translatedText;
     })
     .catch(errorHandler)
